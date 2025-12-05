5b82c8a53a0c5eb14b03d31c88f190f8be2c5926
keepitlight <starry@mail.com>
2025-12-06 01:57:29 +0800

    add log

---
+++ log/zap.go
@@ -0,0 +1,80 @@
+package log
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const messageKey = "message"
+
+type Zap struct {
+	log  *zap.Logger
+	Sync func() error
+}
+
+func (l *Zap) Log(level log.Level, kv ...interface{}) {
+	if len(kv) == 0 || len(kv)%2 != 0 {
+		l.log.Warn("key value must appear in pairs: ", zap.Any("kv", kv))
+		return
+	}
+
+	var data []zap.Field
+	for i := 0; i < len(kv); i += 2 {
+		data = append(data, zap.Any(fmt.Sprint(kv[i]), kv[i+1]))
+	}
+
+	l.log.Log(zapcore.Level(level), "", data...)
+	return
+}
+
+func (l *Zap) Info(kv ...interface{}) {
+	l.Log(log.LevelInfo, kv...)
+}
+func (l *Zap) Warn(kv ...interface{}) {
+	l.Log(log.LevelWarn, kv...)
+}
+func (l *Zap) Error(kv ...interface{}) {
+	l.Log(log.LevelError, kv...)
+}
+func (l *Zap) Fatal(kv ...interface{}) {
+	l.Log(log.LevelFatal, kv...)
+}
+func (l *Zap) Debug(kv ...interface{}) {
+	l.Log(log.LevelDebug, kv...)
+}
+
+// NewZap 创建 Zap 日志工具
+func NewZap(name, file string, level zapcore.Level, maxAge, maxSize, maxBackups int) *Zap {
+	var ws zapcore.WriteSyncer
+
+	if level == zapcore.DebugLevel {
+		ws = zapcore.AddSync(os.Stdout)
+	} else {
+		ws = zapcore.AddSync(&lumberjack.Logger{
+			Filename:   file,
+			MaxAge:     maxAge,
+			MaxSize:    maxSize,
+			MaxBackups: maxBackups,
+		})
+	}
+
+	core := zapcore.NewCore(getEncoder(), ws, level)
+	opt := zap.Fields(zap.String("App", name)) // 服务名
+	return &Zap{log: zap.New(core, zap.AddCaller(), zap.AddCallerSkip(2), opt), Sync: core.Sync}
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "time"
+	encoderConfig.MessageKey = messageKey
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+	encoderConfig.EncodeDuration = zapcore.SecondsDurationEncoder
+	return zapcore.NewJSONEncoder(encoderConfig)
+}
